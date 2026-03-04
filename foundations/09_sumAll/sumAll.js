7c14f76b34dc0e33414f35fdfabfac5faffc4343
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-04 12:31:58 +0200

    Fix sumAll.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(startValue, finishValue) {
+if (startValue <= 0 || finishValue <= 0 || !Number.isInteger(startValue) || !Number.isInteger(finishValue) ) {
+    return "ERROR"
+}
+let start = Math.min(startValue, finishValue);
+let end = Math.max(startValue, finishValue);
+let sum = 0;
+for (let i = start; i <= end; i++) {
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
