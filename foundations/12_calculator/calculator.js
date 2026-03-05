89cc091e1b399249061741513d9299589cbcbcb2
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-05 13:19:38 +0200

    Fix calculator.js

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = array.reduce ((accumulator, currentValue) => accumulator + currentValue, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array.reduce ((accumulator, currentValue) => accumulator * currentValue, 1);
+  return result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = a**b;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	if (fact < 0) {
+    return "ERROR";
+  } else if (fact === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= fact; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
