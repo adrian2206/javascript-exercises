9c91fd8e5d19cee976f7c7f30bbd3ddea084d752
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-05 15:23:15 +0200

    Fix fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+let fibonacciSeq = [0, 1];
+for (let i= 2; i <= index; i++) {
+    fibonacciSeq.push( fibonacciSeq[i - 1] + fibonacciSeq[i - 2]);
+}
+return fibonacciSeq[index];
 };
+
+// index = 3 ; [1,1,2]
 
 // Do not edit below this line
 module.exports = fibonacci;
