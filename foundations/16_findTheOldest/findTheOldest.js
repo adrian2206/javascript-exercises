205db01efea3b74b51656bfd0aa11e820c95d7ca
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-05 17:06:00 +0200

    Fix findTheOldest.js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce ((oldest, person) => {
+        const currentYear = new Date().getFullYear();
 
+        const agePerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const ageOldest = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        return agePerson > ageOldest ? person : oldest;
+    });
 };
 
 // Do not edit below this line
