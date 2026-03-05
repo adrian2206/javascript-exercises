1aec71f5e62c8562e9d91a0ba69c66a453576a43
AdrianP <29433074+adrian2206@users.noreply.github.com>
2026-03-05 13:38:34 +0200

    Fix palindromes.js

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let processedWord = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reverseWord = processedWord.split("").reverse().join("");
+    if (processedWord === reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
